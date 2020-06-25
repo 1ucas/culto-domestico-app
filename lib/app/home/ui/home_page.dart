b163daffb18574b026d79b6351bdcdb3e4fc6c18
Lucas Maciel <lucasramos.m@gmail.com>
2020-06-25 20:45:22 -0300

    Implementação da Home page

--- lib/app/home/ui/home_page.dart
+++ lib/app/home/ui/home_page.dart
@@ -1,7 +1,25 @@
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Home'),),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RaisedButton(onPressed: () => {}, child: Text('Abrir Painel / Perfil Familia / Perfil da Pessoa'),),
+          RaisedButton(onPressed: () => {}, child: Text('Abrir Lista Cultinhos Feitos'),),
+          RaisedButton(onPressed: () => {}, child: Text('Abrir Registro Cultinho'),),  // Botão (  +  )
+          RaisedButton(onPressed: () => {}, child: Text('Abrir Registro Pedidos de Oração'),),
+          RaisedButton(onPressed: () => {}, child: Text('Abrir Sugestões de Leitura'),),
+        ],
+      ),
+    );
   }
 }
+
+/*
+Cultinho -> 1) Hino ?  2) Passagem lida. 3) Quem orou? 4) Pedidos de oração
+ */
