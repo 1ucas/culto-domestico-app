e763e0b84f9b9a68369c2698dfe92991f64a7511
Lucas <lucasramos.m@gmail.com>
2020-07-05 21:23:29 -0300

    Incluindo ID no model de cultinho para exclusão

--- lib/app/cultinhos/models/cultinho.dart
+++ lib/app/cultinhos/models/cultinho.dart
@@ -2,10 +2,12 @@
 import 'package:culto_domestico_app/app/home/ui/models/passagem_biblica.dart';
 import 'package:culto_domestico_app/app/pedidos_oracao/models/pedido_oracao.dart';
 import 'package:culto_domestico_app/app/utils/date/date_utils.dart';
+import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
 
 class Cultinho {
 
+  final String id = UniqueKey().toString();
   final DateTime data;
   final List<PedidoOracao> pedidosOracao;
   final List<PassagemBiblica> leituraFeita;
